5e1b4dde84ba9a59dd4fa7fa9a691322f9f34257
pedroolivagil <onion_oliva@hotmail.com>
2018-03-26 03:32:37 +0200

    v.0.0.55

---
+++ www/php/db_manager.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by OliLogicStudios.
+ * User: OlivaDevelop
+ * Project: rolermaster
+ * File: db_manager.php
+ * Date: 26/03/2018 00:58
+ */
+sleep(1);
+echo '{"result":200,"entities":[{"locale.idLocale":"45","locale.codeISO":"ES","locale_trans.idTrans":"99","locale_trans.idLocale":"1","locale_trans.idLocaleGroup":"45","locale_trans.text":"Spanish"},{"locale.idLocale":"45","locale.codeISO":"ES","locale_trans.idTrans":"59","locale_trans.idLocale":"45","locale_trans.idLocaleGroup":"45","locale_trans.text":"Espa\u00f1ol"}]}';
