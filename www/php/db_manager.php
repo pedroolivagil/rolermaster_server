c13a90c3bc1fc7bf7b12f50a20d1706b26867a62
pedroolivagil <onion_oliva@hotmail.com>
2018-03-28 00:49:23 +0200

    v.0.0.55

--- www/php/db_manager.php
+++ www/php/db_manager.php
@@ -7,4 +7,8 @@
  * Date: 26/03/2018 00:58
  */
 sleep(1);
-echo '{"result":200,"entities":[{"locale.idLocale":"45","locale.codeISO":"ES","locale_trans.idTrans":"99","locale_trans.idLocale":"1","locale_trans.idLocaleGroup":"45","locale_trans.text":"Spanish"},{"locale.idLocale":"45","locale.codeISO":"ES","locale_trans.idTrans":"59","locale_trans.idLocale":"45","locale_trans.idLocaleGroup":"45","locale_trans.text":"Espa\u00f1ol"}]}';
+echo '{"result":200,
+"entities":[
+{"locale.idLocale":"45","locale.codeISO":"ES","locale_trans.idTrans":"99","locale_trans.idLocale":"1","locale_trans.idLocaleGroup":"45","locale_trans.text":"Spanish"},
+{"locale.idLocale":"45","locale.codeISO":"ES","locale_trans.idTrans":"59","locale_trans.idLocale":"45","locale_trans.idLocaleGroup":"45","locale_trans.text":"Espa\u00f1ol"}
+]}';
